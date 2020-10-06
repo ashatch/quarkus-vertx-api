a5cf4d1be5f72997f079231299a992989bc3938e
Andrew Hatch <andrew@andrewhatch.net>
2020-10-06 23:15:36 +0100

    initial commit

---
+++ src/test/java/net/andrewhatch/quarkusvertx/NativeapiIT.java
@@ -0,0 +1,9 @@
+package net.andrewhatch.quarkusvertx;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeapiIT extends apiTest {
+
+    // Execute the same tests but in native mode.
+}
